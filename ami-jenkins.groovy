3be0632f477c269177a05da0ff140a16542de8fd
Rahhul1309 <abbavaram.r@northeastern.edu>
2024-06-11 12:28:21 -0400

    added the code

--- ami-jenkins.groovy
+++ ami-jenkins.groovy
@@ -4,7 +4,7 @@ multibranchPipelineJob('ami-jenkins') {
         id('csye7125-ami-jenkins') // IMPORTANT: use a constant and unique identifier
         remote('https://github.com/cyse7125-su24-team13/ami-jenkins.git')
         credentialsId('github-token')
-        includes('JENKINS-*')
+        includes('*')
     }
   }
 
